6a81033cd7c7e0d9de4615d817d2ab4a554acbc3
alex fernando egidio <alex.fernando.egidio@gmail.com>
2026-06-13 03:47:14 -0300

    Tarefa de controle de estoque

--- src/main/java/teste/Teste.java
+++ src/main/java/teste/Teste.java
@@ -4,6 +4,10 @@ public class Teste {
 	
 	public static void main(String[] args) {
 		System.out.println("Aula de git hub pulll teste");
+		System.out.println("Aula de git hub pulll teste");
+		System.out.println("Aula de git hub pulll teste");
+		System.out.println("Aula de git hub pulll teste");
+		System.out.println("Aula de git hub pulll teste");
 	}
 
 }
