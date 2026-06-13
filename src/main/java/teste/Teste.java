29321bc80e4f8db58d6084e05e7e9913f3f60909
jdevcursojava <notificacao@jdevtreinamento.com.br>
2026-06-13 03:45:00 -0300

    Update Teste.java

--- src/main/java/teste/Teste.java
+++ src/main/java/teste/Teste.java
@@ -3,7 +3,7 @@ package teste;
 public class Teste {
 	
 	public static void main(String[] args) {
-		System.out.println("Aula de git hub ");
+		System.out.println("Aula de git hub pulll teste");
 	}
 
 }
